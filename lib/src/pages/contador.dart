989f3f33d110c6ffde39114af7297f3c83681045
jonascolas <jonas@fusemachines.com>
2022-02-22 13:05:23 -0300

    substract and reset

--- lib/src/pages/contador.dart
+++ lib/src/pages/contador.dart
@@ -13,26 +13,47 @@ class _ContadorState extends State<Contador> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Titulo with Stateful'),
-          centerTitle: true,
+      appBar: AppBar(
+        title: Text('Titulo with Stateful'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Numero de clicks:', style: _texto),
+            Text('$_contar', style: _texto),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('Numero de clicks:', style: _texto),
-              Text('$_contar', style: _texto),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              _contar++;
-            });
-          },
-        ));
+      ),
+      floatingActionButton: _crearBotones(),
+    );
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30.0),
+        FloatingActionButton(
+            child: Icon(Icons.exposure_zero), onPressed: _reset),
+        Expanded(child: SizedBox()),
+        FloatingActionButton(child: Icon(Icons.remove), onPressed: _substract),
+        SizedBox(width: 10.0),
+        FloatingActionButton(child: Icon(Icons.add), onPressed: _add),
+      ],
+    );
+  }
+
+  void _add() {
+    setState(() => _contar++);
+  }
+
+  void _substract() {
+    setState(() => _contar--);
+  }
+
+  void _reset() {
+    setState(() => _contar = 0);
   }
 }
